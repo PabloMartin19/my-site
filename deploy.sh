af881b5c6084530a360285900ca88a685784ab1c
Pablo Martín Hidalgo <pmartinhidalgo19@gmail.com>
2024-10-03 10:26:15 +0200

    chore: kk

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,17 @@ bundle exec jekyll build
 # Navega al repositorio de despliegue
 cd /home/pavlo/github/pablomartin19.github.io
 
-# Limpia el contenido anterior (opcional, pero recomendado)
-git rm -rf .
-git clean -fxd
+# Limpia el contenido anterior (manteniendo .git)
+find . -maxdepth 1 ! -name .git ! -name . ! -name .. -exec rm -rf {} +
 
 # Copia el contenido generado al repositorio de despliegue
-cp -R /home/pavlo/github/pablomartin19.github.io/_site* .
+cp -R /home/pavlo/web/jekyll-theme-chirpy/_site .
 
 # Añade los cambios al repositorio de despliegue
 git add .
 
 # Haz commit de los cambios
-git commit -m "chore: actualización del blog $(date)"
+git commit -m "Actualización del sitio $(date)"
 
 # Empuja los cambios a GitHub
 git push origin main
